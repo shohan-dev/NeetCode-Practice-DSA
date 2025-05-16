4c749e24e331f7c420cfc8d4ebd765f04df995be
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:14 +0600

    5

--- 1. Array/5. Replace Elements With Greatest Element On Right Side.py
+++ 1. Array/5. Replace Elements With Greatest Element On Right Side.py
@@ -1,1 +1,10 @@
-# Replace Elements With Greatest Element On Right Side
+class Solution:
+    def replaceElements(self, a: List[int]) -> List[int]:
+        max_val = -1
+
+        for i in range(len(a)-1,-1,-1):
+            current = a[i]
+            a[i] = max_val
+            max_val = max(current,max_val)
+        return a
+        
