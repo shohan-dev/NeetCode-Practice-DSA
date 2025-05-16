d76dbda7f347c2895eaf97163ba2c9dc2ab7989c
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:30 +0600

    8

--- 1. Array/8. Length of Last Word.py
+++ 1. Array/8. Length of Last Word.py
@@ -1,1 +1,20 @@
-# Length of Last Word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+        count = 0
+
+        # Step 1: Skip trailing spaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        # Step 2: Count characters in the last word
+        while i >= 0 and s[i] != ' ':
+            count += 1
+            i -= 1
+
+        return count
+
+# another way
+# s.strip()
+# word = s.split()
+# return len(word[-1])
