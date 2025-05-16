b4d9c7b5fa84d5e7aea13c0445f83f8e7517e77a
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:17 +0600

    6

--- 1. Array/6. Is Subsequence.py
+++ 1. Array/6. Is Subsequence.py
@@ -1,1 +1,13 @@
-# Is Subsequence
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_pointer, t_pointer = 0,0
+        if len(s) == 0:
+            return True
+
+        while t_pointer < len(t):
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer +=1
+            if s_pointer == len(s):
+                return True
+            t_pointer +=1
+        return False
