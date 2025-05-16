a4c3b8641a92682c0bae462719d1dc1fe76a777f
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:04 +0600

    3

--- 1. Array/3. Contains Duplicate.py
+++ 1. Array/3. Contains Duplicate.py
@@ -1,1 +1,9 @@
-# Contains Duplicate
+class Solution:
+    def containsDuplicate(self, a: List[int]) -> bool:
+        seen = set()
+        for i in a:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+        
