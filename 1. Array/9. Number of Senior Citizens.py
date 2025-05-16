1abd20bfa7da9244349c40f911cb47123eb918ed
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:35 +0600

    9

--- 1. Array/9. Number of Senior Citizens.py
+++ 1. Array/9. Number of Senior Citizens.py
@@ -1,1 +1,8 @@
-# Number of Senior Citizens
+class Solution:
+    def countSeniors(self, a: List[str]) -> int:
+        count =0
+        for i in a:
+            if 60< int(i[11:13]):
+                count +=1
+
+        return count
