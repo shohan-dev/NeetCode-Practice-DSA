6e7169b2cbe39ab32a53bf9c6de493ff9ab8a017
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-16 16:30:09 +0600

    4

--- 1. Array/4. Valid Anagram.py
+++ 1. Array/4. Valid Anagram.py
@@ -1,1 +1,20 @@
-# Valid Anagram
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        table_s = {}
+        table_t = {}
+
+        for i in s:
+            if i in table_s:
+                table_s[i] +=1
+            else:
+                table_s[i] = 1
+
+        for i in t:
+            if i in table_t:
+                table_t[i] +=1
+            else:
+                table_t[i] = 1
+
+        return table_s==table_t
+
+        
