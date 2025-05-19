01ade92aa519142dc211d8af4d1d5fb2bea49708
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-19 22:58:54 +0600

    10

--- 1. Array/10. Two Sum.py
+++ 1. Array/10. Two Sum.py
@@ -1,1 +1,11 @@
-# Two Sum
+class Solution:
+    def twoSum(self, a: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(a)):
+            c = target - a[i]
+            if c in seen:
+                return [seen[c],i]
+            seen[a[i]] = i
+        
+            
+        
