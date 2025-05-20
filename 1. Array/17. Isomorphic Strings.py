9706f6fa708438b12f877b0c51b57ff1d3b65f5c
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:58:29 +0600

    17

--- 1. Array/17. Isomorphic Strings.py
+++ 1. Array/17. Isomorphic Strings.py
@@ -1,1 +1,19 @@
-# Isomorphic Strings
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s))==len(set(t))==len(set(zip(s,t)))  # check same or diff
+
+
+
+
+
+
+# Another
+        # s_map, t_map = {},{}
+
+        # for i,j in zip(s,t):
+        #     if (i in s_map and s_map[i] != j) or (j in t_map and t_map[j] != i):
+        #         return False
+        #     s_map[i] = j
+        #     t_map[j] = i
+        # return True
+        
