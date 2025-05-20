d3c588c759e68552700356fee425261287e5e013
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:19:30 +0600

    13

--- 1. Array/13. Group Anagrams.py
+++ 1. Array/13. Group Anagrams.py
@@ -1,1 +1,10 @@
-# Group Anagrams
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, a: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for i in a:
+            key = "".join(sorted(i))
+            res[key].append(i)
+
+        return list(res.values())
