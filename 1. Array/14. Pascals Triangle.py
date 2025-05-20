fb429bd4d05867b02730aa3c590516588c25c3b6
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:19:37 +0600

    14

--- 1. Array/14. Pascals Triangle.py
+++ 1. Array/14. Pascals Triangle.py
@@ -1,1 +1,12 @@
-# Pascals Triangle
+import math
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        final = []
+        for j in range(1, numRows + 1):
+            res = []
+            for i in range(1, j + 1):
+                res.append(math.comb(j - 1, i - 1))
+            final.append(res)
+        return final
