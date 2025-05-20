0dd9bdbd6d879dbb8ff4b2bbef19a4146a712705
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:19:40 +0600

    15

--- 1. Array/15. Remove Element.py
+++ 1. Array/15. Remove Element.py
@@ -1,1 +1,10 @@
-# Remove Element
+from typing import List
+
+class Solution:
+    def removeElement(self, a: List[int], val: int) -> int:
+        k = 0 
+        for i in range(len(a)):
+            if a[i] != val:
+                a[k] = a[i]
+                k += 1
+        return k
