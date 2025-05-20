2d459bff266870aa2a9721ba6f342eea5af65fc8
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:19:44 +0600

    16

--- 1. Array/16. Unique Email Addresses.py
+++ 1. Array/16. Unique Email Addresses.py
@@ -1,1 +1,12 @@
-# Unique Email Addresses
+from typing import List
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        found = set()
+
+        for email in emails:
+            local, domain = email.split('@', 1)
+            cleaned_local = local.partition('+')[0].replace('.', '') 
+            found.add(f"{cleaned_local}@{domain}")
+
+        return len(found)
