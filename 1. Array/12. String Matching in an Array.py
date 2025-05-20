4cbad5c0e29e153d752e2fee6ab99f71031443fe
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-20 21:19:26 +0600

    12

--- 1. Array/12. String Matching in an Array.py
+++ 1. Array/12. String Matching in an Array.py
@@ -1,1 +1,11 @@
-# String Matching in an Array
+class Solution:
+    def stringMatching(self, a: List[str]) -> List[str]:
+            a.sort(key=len)
+            res = []
+            for i in range(len(a)):
+                for j in range(i+1,len(a)):
+                    if a[i] in a[j]:
+                        res.append(a[i])
+                        break
+
+            return res
