80dc48e9d8ceb39881c1720049efe49af1c4859d
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-24 22:56:26 +0600

    18

--- 1. Array/18. Can Place Flowers.py
+++ 1. Array/18. Can Place Flowers.py
@@ -1,1 +1,16 @@
-# Can Place Flowers
+class Solution:
+    def canPlaceFlowers(self, a: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        a = [0] + a + [0]
+
+        for i in range(1,len(a)-1):
+            if a[i] == 0:
+                if a[i-1] ==0 and a[i+1] ==0:
+                    a[i] = 1
+                    n -=1
+                    if n==0:
+                        return True
+                    
+
+        return False
