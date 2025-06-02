c258c4940bd22118028683d874475c4a55f6e00d
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-03 00:20:55 +0600

    19

--- 1. Array/19. Majority Element.py
+++ 1. Array/19. Majority Element.py
@@ -1,1 +1,18 @@
-# Majority Element
+from collections import Counter
+class Solution:
+    def majorityElement(self, a: List[int]) -> int:
+        count = Counter(a) # return a dict
+        max_num = max(count, key=count.get)
+        return max_num
+
+
+# Another
+        # count = {}
+
+        # for i in a:
+        #     if i in count:
+        #         count[i] +=1
+        #     else:
+        #         count[i] = 1
+
+        # return max(count,key=count.get)
