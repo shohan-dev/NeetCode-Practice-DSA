cc76389b2c7f3526af8dab5b34c5a3d41f371393
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-03 00:21:04 +0600

    22

--- 1. Array/22. Maximum Ascending Subarray Sum.py
+++ 1. Array/22. Maximum Ascending Subarray Sum.py
@@ -1,1 +1,13 @@
-# Maximum Ascending Subarray Sum
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        summ=nums[0]
+        maxSum=0
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                summ+=nums[i]
+            else:
+                maxSum=max(maxSum,summ)
+                summ=nums[i]
+        maxSum=max(summ,maxSum)
+        return maxSum
+        
