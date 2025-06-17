c6499841034b5386dcfc3eccf9c1a7aba4c758cf
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-17 23:26:34 +0600

    23

--- 1. Array/23. Find Pivot Index.py
+++ 1. Array/23. Find Pivot Index.py
@@ -1,1 +1,10 @@
-# Find Pivot Index
+class Solution:
+    def pivotIndex(self, a: List[int]) -> int:
+        total = sum(a)
+        left_sum = 0
+
+        for i in range(len(a)):
+            if left_sum == total - left_sum - a[i]:
+                return i
+            left_sum += a[i]
+        return -1
