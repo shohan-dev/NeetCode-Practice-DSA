3915f5467a2a870d961d57eed500daa11093d256
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-17 23:26:51 +0600

    27

--- 1. Array/27. Find Missing and Repeated Values.py
+++ 1. Array/27. Find Missing and Repeated Values.py
@@ -1,1 +1,20 @@
-# Find Missing and Repeated Values
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        seen = set()
+        duplicate = 0
+        missing = 0
+        count =0
+        for i in grid:
+            for j in i:
+                count +=1
+                if j in seen:
+                    duplicate = j
+                seen.add(j)
+
+        for i in range(1,count+1):
+            if i in seen:
+                continue
+            missing =i
+            break
+
+        return [duplicate,missing]
