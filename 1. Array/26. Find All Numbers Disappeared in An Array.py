66a11caa7e28abbe561e2b3ff4ee534575dac9b7
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-17 23:26:47 +0600

    26

--- 1. Array/26. Find All Numbers Disappeared in An Array.py
+++ 1. Array/26. Find All Numbers Disappeared in An Array.py
@@ -1,1 +1,11 @@
-# Find All Numbers Disappeared in An Array
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        s = set(nums)
+        not_found = []
+        for i in range(1,n+1):
+            if i in s:
+                continue
+            not_found.append(i)
+        return not_found
+        
