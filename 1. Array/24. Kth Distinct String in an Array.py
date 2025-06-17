dd57489f6762ac64f4cf0bf77467aada176dd134
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-17 23:26:40 +0600

    24

--- 1. Array/24. Kth Distinct String in an Array.py
+++ 1. Array/24. Kth Distinct String in an Array.py
@@ -1,1 +1,9 @@
-# Kth Distinct String in an Array
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        count = Counter(arr)
+        res = [key for key, value in count.items() if value == 1]
+        if len(res) >= k:
+            return res[k-1]
+        else:
+            return ""
+        
