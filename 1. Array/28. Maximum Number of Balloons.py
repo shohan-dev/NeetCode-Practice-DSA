f075e0667dcf76b65f0723a93ae6d7f8cb024f84
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-17 23:26:55 +0600

    28

--- 1. Array/28. Maximum Number of Balloons.py
+++ 1. Array/28. Maximum Number of Balloons.py
@@ -1,1 +1,8 @@
-# Maximum Number of Balloons
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b = text.count('b')
+        a = text.count('a')
+        l = text.count('l')//2
+        o = text.count('o')//2
+        n = text.count('n')
+        return min(b,a,l,o,n)
