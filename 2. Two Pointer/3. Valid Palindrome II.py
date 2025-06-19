65bdd4104c4ce7f4925634c3a027d234fc637442
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-19 23:12:34 +0600

    3

--- 2. Two Pointer/3. Valid Palindrome II.py
+++ 2. Two Pointer/3. Valid Palindrome II.py
@@ -1,1 +1,11 @@
-# Valid Palindrome II
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                skip_l = s[l+1:r+1]  # remove left
+                skip_r = s[l:r]      # remove right
+                return skip_l == skip_l[::-1] or skip_r == skip_r[::-1]
+            l += 1
+            r -= 1
+        return True
