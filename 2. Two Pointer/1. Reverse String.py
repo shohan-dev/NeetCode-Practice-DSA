8eb65127e7d22098aa18291ac21dc82a917668d5
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-19 23:12:28 +0600

    Two Pointer Probelm 1

--- 2. Two Pointer/1. Reverse String.py
+++ 2. Two Pointer/1. Reverse String.py
@@ -1,1 +1,7 @@
-# Reverse String
+from typing import List
+
+class Solution:
+    def reverseString(self, a: List[str]) -> None:
+        n = len(a)
+        for i in range(n//2):
+            a[i],a[n-1-i] = a[n-1-i],a[i]
