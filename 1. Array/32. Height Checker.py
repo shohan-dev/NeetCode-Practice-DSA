5d0b2e3e0f0448c79956c7066afa32f3589485bf
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-19 23:09:02 +0600

    32

--- 1. Array/32. Height Checker.py
+++ 1. Array/32. Height Checker.py
@@ -1,1 +1,9 @@
-# Height Checker
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        res = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != res[i]:
+                count +=1
+
+        return count
