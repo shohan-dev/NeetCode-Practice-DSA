a5be452d58682ed82180db3a8d08e185a9e73453
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-06-19 23:09:09 +0600

    33

--- 1. Array/33. Special Array I.py
+++ 1. Array/33. Special Array I.py
@@ -1,1 +1,6 @@
-# Special Array I
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums)-1):
+            if nums[i] %2 == nums[i+1] %2:
+                return False
+        return True
